412536e48e25b3e17662c5e33017d283e8873b3d
DoughertyWM <wmd6642@rit.edu>
2014-03-11 16:05:16 -0400

    Added code to Response codes

--- src/fuzzer/ResponseCodes.java
+++ src/fuzzer/ResponseCodes.java
@@ -1,6 +1,20 @@
 package fuzzer;
+
 import com.gargoylesoftware.htmlunit.*;
+import java.net.*;
+import java.util.ArrayList;
 
 public class ResponseCodes {
+	
+	//not sure if this should be looking through the HTML Anchors ur just at urls
+	//Feel free to change this obviously
+	public void getResponseCode(ArrayList<HtmlAnchor> links){
+		for (HtmlAnchor link : links){
+			if (getStatusCode(link)!= 200) {
+				//Report it?
+				//Not exactly sure what he wants us to do when he says "Report it."
+			}
+		}
+	}
 	
 }
